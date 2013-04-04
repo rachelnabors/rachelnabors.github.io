a20a5dba84184afa29c451016252d22ea9558a20
Rachel Nabors <rachel@thepinkcrow.com>
2013-04-04 18:31:25 -0400

    Trying to get compass working.

---
+++ _scss/config.rb
@@ -0,0 +1,22 @@
+# Require any additional compass plugins here.
+require "susy"
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
+
+# Local development paths
+css_dir = "_site/css"
+images_dir = "_site/img"
+javascripts_dir = "_site/js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
