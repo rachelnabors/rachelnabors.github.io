473a08f74521e7ef991fff380dda430a8efc5328
Rachel Nabors <rachel@thepinkcrow.com>
2013-05-02 18:39:25 -0400

    Start watching _site for generated content.

---
+++ _site/config.rb
@@ -0,0 +1,30 @@
+# Require any additional compass plugins here.
+require "susy"
+
+# Set this to the root of your project when deployed:
+sass_dir = "_scss"
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/img"
+http_css_path = "/css"
+
+# Local development paths
+css_dir = "/css"
+images_dir = "/img"
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass _scss scss && rm -rf sass && mv scss sass
+
