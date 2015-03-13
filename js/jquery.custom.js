dea59551c9a16bcc6e75321c916216e970351e7d
Rachel Nabors <rachelnabors@gmail.com>
2015-03-13 16:06:52 -0700

    Adding velocity.js for scrolling animations.

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -118,6 +118,22 @@
         //     })
         //   });
         // });
+
+        // Velocity bind click event to all internal page anchors
+        $("#content").on("click", "a[href*=#]", function(e) {
+          // prevent default action and bubbling
+          e.preventDefault();
+          e.stopPropagation();
+          // set target to anchor's "href" attribute
+          var target = $(this).attr("href");
+          // scroll to each target
+          $(target).velocity("scroll", {
+              duration: 500,
+              offset: -60,
+              easing: "ease-in-out"
+          });
+        });
+
     });
 
 })(jQuery);
