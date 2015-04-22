61cfc11710c034c072cf0036770a7c0a8a3a83e2
Rachel Nabors <rachelnabors@gmail.com>
2015-04-22 10:57:07 -0700

    Fixing transitions on PJAX

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -51,94 +51,47 @@
             }
         });
 
-        // Training page's workshop toggle
-        // function toggleWorkshop(target) {
-        //     if ( target.attr('data-open') === "true" ) {
-        //       target.attr('data-open', "false");
-        //       $(this).html('<span data-icon="&#10025;" class="icon"></span>');
-        //     } else {
-        //       target.attr('data-open', 'true');
-        //       $(this).html("close");
-        //     }
-        // }
+        /* PJAXIN'! */
+        if ($.support.pjax) {
+            $(document).on("click", "a[href^='"+siteURL+"'], a[href^='/'], a[href^='./'], a[href^='../'], a[href^='#'], a:not(a[target='_blank'])", function(e){
 
-// function(){
-//                     if ( target.attr('data-open') === "true" ) {
-//                       target.attr('data-open', "false");
-//                       $(this).html('<span data-icon="&#10025;" class="icon"></span>');
-//                     } else {
-//                       target.attr('data-open', 'true');
-//                       $(this).html("close");
-//                     }
-//                 })
+                e.preventDefault();
 
-        $(document).pjax("a[href^='"+siteURL+"'], a[href^='/'], a[href^='./'], a[href^='../'], a[href^='#'], a:not(a[target='_blank'])", "#content", {fragment:"#content"});
+                $body.attr('data-page-status', 'loading');
+                // Vars
+                var url = $(this).attr("href");
 
-        // This version does NOT pjax-ify the back button
-        // $body.on('click', "a[href^='"+siteURL+"'], a[href^='/'], a[href^='./'], a[href^='../'], a[href^='#'], a:not(a[target='_blank'])", function(event) {
-        //         var clickery = event;
-        //         // .loading transitions opacity to 0
-        //         $body.attr('data-page-status', 'loading');
+                $content.one(window.transitionEnd, function(){
+                    $.pjax({
+                        url: url,
+                        container: "#content",
+                        fragment: "#content"
+                    });
+                });
+            });
 
-        //         if (window.transitionEnd) {
-        //             // trigger pjax post-transition only for one transitionend
-        //             if (e.originalEvent.propertyName === "opacity") {
-        //                 $("#content").on(window.transitionEnd, function(e){
-        //                     $.pjax({
-        //                         url: clickery.target.href,
-        //                         container: '#content',
-        //                         fragment: '#content'
-        //                     });
-        //                 });
-        //             }
-        //         } else {
-        //             // a crappy timeout if no transitionend
-        //             setTimeout( function(){
-        //                 $.pjax.click(clickery,{
-        //                     url: clickery.target.href,
-        //                     container: '#content',
-        //                     fragment: '#content'
-        //                 });
-        //             }, customPjaxTimeout);
-        //         }
-
-        //     // STOP THE LINK FROM WORKING NORMALLY
-        //     return false;
-        // });
-        $(document).on('pjax:start', function() {
-            $body.attr('data-page-status', 'loading');
-        });
-        $(document).on('pjax:end', function(event) {
-            $body.attr('data-page-status', 'loaded');
-            // On smaller screens, close the nav bar
-            if (window.matchMedia(mediaUpToSidebar).matches) {
-                if ( $html.attr('data-masthead') === "open" ) {
-                  $html.attr('data-masthead', "closed");
+            $(document).on('pjax:end', function(event) {
+                $body.attr('data-page-status', 'loaded');
+                // On smaller screens, close the nav bar
+                if (window.matchMedia(mediaUpToSidebar).matches) {
+                    if ( $html.attr('data-masthead') === "open" ) {
+                      $html.attr('data-masthead', "closed");
+                    }
                 }
-            }
-            // Give body class based on location to highlight nav
-            var loc = window.location.pathname.split('/');
-            var dir = loc[1];
-            // Posts have numeric root folders.
-            // check to see if the directory ISN'T numeric
-            if (isNaN(dir)) {
-                $body.attr('data-location', dir);
-            } else if (!dir) {
-                $body.attr('data-location', 'home');
-            } else {
-                $body.attr('data-location', 'post');
-            }
-        });
-
-        // Play that funky JS.
-        // https://github.com/defunkt/jquery-pjax/issues/469
-        // $(document).on("pjax:popstate", function() {
-        //   $(document).on("pjax:end", function(event) {
-        //     $(event.target).find("script[data-exec-on-popstate]").each(function() {
-        //       $.globalEval(this.text || this.textContent || this.innerHTML || '');
-        //     })
-        //   });
-        // });
+                // Give body class based on location to highlight nav
+                var loc = window.location.pathname.split('/');
+                var dir = loc[1];
+                // Posts have numeric root folders.
+                // check to see if the directory ISN'T numeric
+                if (isNaN(dir)) {
+                    $body.attr('data-location', dir);
+                } else if (!dir) {
+                    $body.attr('data-location', 'home');
+                } else {
+                    $body.attr('data-location', 'post');
+                }
+            });
+        }
 
         // Velocity bind click event to all internal page anchors
         $("#content").on("click", "a[href*=#]", function(e) {
